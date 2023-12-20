96e025236dadf973be174ac905bf57bd0c715fd2
Desira Wijesundara <desirasenal@gmail.com>
2023-12-20 13:28:34 +0530

    Add player death logic, traps and moving platforms

---
+++ Game/Assets/Scripts/Player_Life.cs
@@ -0,0 +1,34 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Player_Life : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("Death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
