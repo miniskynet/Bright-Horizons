d16ea616408e2f34ce600ebc67b9f8e09ed78de3
Desira Wijesundara <desirasenal@gmail.com>
2024-02-20 17:59:46 +0530

    Create random item generation
    Requires an algorithm to detect terrain and spawn objects on top of terrain

---
+++ Game/Assets/Scripts/Procedural_Generation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class Procedural_Generation : MonoBehaviour
+{
+    [SerializeField] private GameObject item;
+    [SerializeField] private Transform itemList;
+
+    void Start()
+    {
+       ItemGeneration(); 
+    }
+
+    private void ItemGeneration()
+    {
+        for (int i = 0; i < 10; ++i) {
+            Vector3 position = new Vector3(Random.Range(0, 90),2,0);
+            Instantiate(item, position, Quaternion.identity, itemList);
+    }
+    }
+
+
+}
